e676ba3c5e2501b624248904b8614104da5f86bc
mawsyh <moh.mir36@gmail.com>
2021-12-06 20:24:50 +0330

    Q4 done!

--- Q2/server.js
+++ Q2/server.js
@@ -4,5 +4,8 @@ const app = express();
 const port = 5000;
 
 app.use(express.static(path.join(__dirname, "public")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "notfound.html"));
+});
 
 app.listen(port);
---
+++ Q4/public/users.js
@@ -0,0 +1,36 @@
+const users = [
+  {
+    username: "Ali",
+    age: 10,
+  },
+  {
+    username: "Reza",
+    age: 20,
+  },
+  {
+    username: "Ahmad",
+    age: 3,
+  },
+  {
+    username: "Akbar",
+    age: 12,
+  },
+  {
+    username: "Sina",
+    age: 51,
+  },
+  {
+    username: "Maryam",
+    age: 29,
+  },
+  {
+    username: "Mahsa",
+    age: 23,
+  },
+  {
+    username: "Rahim",
+    age: 22,
+  },
+];
+
+module.exports = users;
