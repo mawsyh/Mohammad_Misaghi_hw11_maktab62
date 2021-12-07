7641c9ef8868a25287fa8915f81e94f5101b6a21
mawsyh <moh.mir36@gmail.com>
2021-12-07 13:13:26 +0330

    Q4 completed and some routings

--- Q4/public/users.js
+++ Q4/public/users.js
@@ -2,34 +2,42 @@ const users = [
   {
     username: "Ali",
     age: 10,
+    status: "active",
   },
   {
     username: "Reza",
     age: 20,
+    status: "deactive",
   },
   {
     username: "Ahmad",
     age: 3,
+    status: "active",
   },
   {
     username: "Akbar",
     age: 12,
+    status: "active",
   },
   {
     username: "Sina",
     age: 51,
+    status: "deactive",
   },
   {
     username: "Maryam",
     age: 29,
+    status: "deactive",
   },
   {
     username: "Mahsa",
     age: 23,
+    status: "active",
   },
   {
     username: "Rahim",
     age: 22,
+    status: "deactive",
   },
 ];
 
--- Q4/server.js
+++ Q4/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 const port = 5000;
+const path = require("path");
 const user = require("./routes/api/user");
 const admin = require("./routes/api/admin");
 
+//middleware
 app.use("/user", user);
 app.use("/admin", admin);
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", (req, res) => {
