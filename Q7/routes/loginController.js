fdc2f0cd10c560ac5dfefc4fb1c708c796506762
mawsyh <moh.mir36@gmail.com>
2021-12-08 17:03:45 +0330

    Q7 commenting

--- Q7/routes/loginController.js
+++ Q7/routes/loginController.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
 const users = require("../model/users.json");
 
+// load login page by ejs and set error message visibility to none
 router.get("/", (req, res) => {
   res.render("login.ejs", { visibility: "hide" });
 });
 
+// check for errors on login and change visibility of error message
 router.post("/", (req, res) => {
   const chosenUser = users.find((user) => user.username === req.body.username);
   if (chosenUser) {
--- Q7/routes/registerController.js
+++ Q7/routes/registerController.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-
 //The fs.promises API provides an alternative
 //set of asynchronous file system methods
 // that return Promise objects rather than using callbacks
 const fsPromises = require("fs").promises;
+// making a local DB by react syntax
 const usersDB = {
   users: require("../model/users.json"),
   setUser: function (data) {
@@ -13,10 +13,12 @@ const usersDB = {
   },
 };
 
+// render page by ejs
 router.get("/", (req, res) => {
   res.render("register.ejs");
 });
 
+// make new user as the user fill the input
 router.post("/", async (req, res) => {
   try {
     const newUser = {
@@ -25,14 +27,17 @@ router.post("/", async (req, res) => {
       email: req.body.email,
       password: req.body.password,
     };
+    //set the user json file to the prev and add new user
     usersDB.setUser([...usersDB.users, newUser]);
     await fsPromises.writeFile(
       path.join(__dirname, "../model/users.json"),
       JSON.stringify(usersDB.users)
     );
     console.log(usersDB.users);
-    await res.redirect("/login");
+    // if register was successfull go to login page
+    res.redirect("/login");
   } catch (e) {
+    // if any error happens reload the page
     console.log(e);
     res.redirect("/register");
   }
